468e3652da5762e2eff25eb048a38d8cc1df54b2
Suganthi Jayaseelan <suganthi1483@gmail.com>
2018-08-28 02:47:57 +0000

    CLI Add 2 nos

---
+++ w1d1/talkingCalendar.js
@@ -0,0 +1,7 @@
+var talkingCalendar = function(date) {
+  // Your code here
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
